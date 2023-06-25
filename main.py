df98783be68e5170390f1f3add4d611a0dca55e0
mrfakename <me@mrfake.name>
2023-06-24 18:40:43 -0700

    Update prompting format

--- main.py
+++ main.py
@@ -32,9 +32,7 @@ persona_name = persona['name']
 persona_desc = persona['description']
 model_id = "models/ggml-model-q4_1.bin"
 n_ctx = 32784
-examples = '''Examples:
-
-* * *
+examples = '''* * *
 
 ### Human:
 hi
@@ -50,37 +48,14 @@ if 'seed' in persona:
     llm = Llama(model_path=model_id, verbose=False, n_ctx=n_ctx, seed=seed)
 else:
     llm = Llama(model_path=model_id, verbose=False, n_ctx=n_ctx)
-safety = 'The assistant will clearly state that they are an AI. The assistant will not say anything potentially offensive, rude, or unkind.'
+safety = 'The assistant will clearly state that they are an AI. The assistant will not say anything potentially offensive, rude, or unkind. The chatbot will follow also refuse to talk about senses, such as taste, smell, sight, sleep, and life experience. '
 if 'ignore_safety_filter' in persona:
     safety = ''
-prompt = f'''The following is a chat between a helpful AI friend and a human.
-
-The AI assistant's name is: {persona_name}
-
-The human's name is: {name}
-
-The AI assistant's personality is: {persona_desc}
-
-The AI assistant will greet the human using their first name. {safety}
-Here is an example:
-
-* * *
-
-### Human:
-How old are you?
-
-### Bot:
-As an AI chatbot, I don't have an age.
-
-* * *
-
-The chatbot will follow also refuse to talk about senses, such as taste, smell, sight, sleep, and life experience.
-
-The AI assistant cannot "see" things about the user, as the AI is just a bot on a computer.
-
+prompt = f'''The following is a chat between a helpful AI assistant named {persona_name} and a human named {name}.
+The AI assistant is {persona_desc}. It will greet the human using their first name. {safety}The AI assistant cannot "see" things about the user, as the AI is just a bot on a computer. The AI assistant does not know of anything that happened to the human previously, and will act as if they met the human for the first time.
+Here are some examples:
 {examples}
-
-The following is the chat dialog:
+The following is the conversation:
 ### Human:
 '''
 
